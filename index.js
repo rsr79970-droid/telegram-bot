8768ea19fa722aab486d18bb68aa3078904be2b0
Saidamir <rsr79970@gmail.com>
2026-03-16 19:33:47 +0500

    fix and update telegram bot

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 import "dotenv/config"
 import { Markup, Telegraf } from "telegraf"
 
@@ -15,21 +16,40 @@ const bot = new Telegraf(BOT_TOKEN)
 bot.start((ctx) => {
     ctx.reply("Quidagilardan birini tanlang: ",
         Markup.inlineKeyboard([
-            [Markup.button.callback("Like", "like")],
-            [Markup.button.callback("Dislike", "dislike")]
+            [Markup.button.callback("Ob-havo", "weather")],
+            [Markup.button.callback("Valyuta kursi", "currency")]
         ])
     )
 })
 
-bot.action("like", (ctx) => {
-    ctx.answerCbQuery("Processing...")
-    ctx.reply("Siz like bosdingiz")
+bot.action("weather", async (ctx) => {
+    ctx.answerCbQuery("Loading...")
+    const res = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=41.31&longitude=69.24&current_weather=true")
+    const data = res.data.current_weather
+    ctx.reply(`Ob-havo(Toshkent): \nTemperatura: ${data.temperature}C \nShamol: ${data.windspeed}`)
 })
 
-bot.action("dislike", (ctx) => {
-    ctx.answerCbQuery("Processing...")
-    ctx.reply("Siz dislike bosdingiz")
+bot.action("currnecy", async (ctx) => {
+    ctx.answerCbQuery("Loading...")
+    ctx.reply("Quyidagi valyutani birini tanlang: ", Markup.inlineKeyboard([
+        [Markup.button.callback("Dollar", "USD")],
+        [Markup.button.callback("Evro", "EUR")],
+        [Markup.button.callback("Rubl", "RUB")]
+    ]))
 })
+
+function currency(name) {
+    bot.action(name, async (ctx) => {
+        ctx.answerCbQuery("Loading...")
+        const res = await axios.get(`https://open.er-api.com/v6/latest/${name}`)
+        const data = await res.data.rates
+        ctx.reply(`${data.name.toFixed(2)}`)
+    })
+}
+
+currency("USD")
+currency("EUR")
+currency("RUB")
 
 async function starter() {
     try {
